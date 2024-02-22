7f7213100457ce1f1a2766818a9420cc6fab65ff
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2024-02-22 15:31:02 -0300

    feat: bring api data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceRickAndMortyService } from '../service/service-rick-and-morty.service';
 
+interface APIPagesInfo {
+  info: {
+    count: number;
+
+    pages: number;
+
+    next: string | null;
+
+    prev: string | null;
+  };
+}
+
+export interface CharacterResponse extends APIPagesInfo {
+  results: RickAndMortyCharacter[];
+}
+
+export interface RickAndMortyCharacter {
+  id: number;
+
+  name: string;
+
+  species: string;
+
+  status: string;
+
+  gender: string;
+
+  image: string;
+
+  origin: {
+    name: string;
+
+    url: string;
+  };
+
+  location: {
+    name: string;
+
+    url: string;
+  };
+
+  episode: string[];
+}
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private serviceRickAndMortyService: ServiceRickAndMortyService) {}
+  data: CharacterResponse = {} as CharacterResponse;
+  ngOnInit(): void {
+    this.serviceRickAndMortyService.getData().subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
